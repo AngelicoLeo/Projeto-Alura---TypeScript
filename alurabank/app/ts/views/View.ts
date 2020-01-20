17e715537b458513894c9b462b5c73851813325f
LeonardoAngelico <leoangelicox@gmail.com>
2020-01-20 10:27:39 -0300

    transformando as classes em modulos do ES2015

--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,19 +1,17 @@
-namespace Views{
-    export abstract class View<T> {
-            private _elemento:JQuery;
-                
-            constructor (seletor:string){
-
-                this._elemento = $(seletor);
-                
-            }
-
-            update(model:T):void{
-
-                this._elemento.html(this.template(model));
-            }
-
-            abstract template(model:T):string;
+export abstract class View<T> {
+        private _elemento:JQuery;
             
-    }
+        constructor (seletor:string){
+
+            this._elemento = $(seletor);
+            
+        }
+
+        update(model:T):void{
+
+            this._elemento.html(this.template(model));
+        }
+
+        abstract template(model:T):string;
+        
 }
